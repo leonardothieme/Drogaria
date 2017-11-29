d4cca4696e4a5bf4cebfa9464a949fc19afdd01a
Leonardo Thieme <leonardothieme95@gmail.com>
2017-11-29 00:08:19 -0200

    [Programação Web com Java] 238 - Alias

--- Drogaria/src/main/java/br/pro/delfino/drogaria/bean/VendaBean.java
+++ Drogaria/src/main/java/br/pro/delfino/drogaria/bean/VendaBean.java
@@ -3,6 +3,7 @@ package br.pro.delfino.drogaria.bean;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -12,16 +13,33 @@ import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
+import br.pro.delfino.drogaria.dao.ClienteDAO;
+import br.pro.delfino.drogaria.dao.FuncionarioDAO;
 import br.pro.delfino.drogaria.dao.ProdutoDAO;
+import br.pro.delfino.drogaria.domain.Cliente;
+import br.pro.delfino.drogaria.domain.Funcionario;
 import br.pro.delfino.drogaria.domain.ItemVenda;
 import br.pro.delfino.drogaria.domain.Produto;
+import br.pro.delfino.drogaria.domain.Venda;
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
 public class VendaBean implements Serializable {
+	private Venda venda;
+	
 	private List<Produto> produtos;
 	private List<ItemVenda> itensVenda;
+	private List<Cliente> clientes;
+	private List<Funcionario> funcionarios;
+	
+	public Venda getVenda() {
+		return venda;
+	}
+	
+	public void setVenda(Venda venda) {
+		this.venda = venda;
+	}
 
 	public List<Produto> getProdutos() {
 		return produtos;
@@ -38,14 +56,34 @@ public class VendaBean implements Serializable {
 	public void setItensVenda(List<ItemVenda> itensVenda) {
 		this.itensVenda = itensVenda;
 	}
+	
+	public List<Cliente> getClientes() {
+		return clientes;
+	}
+	
+	public void setClientes(List<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+	
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+	
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
 
 	@PostConstruct
-	public void listar() {
+	public void novo() {
 		try {
+			venda = new Venda();
+			venda.setPrecoTotal(new BigDecimal("0.00"));
+			venda.setHorario(new Date());
+			
 			ProdutoDAO produtoDAO = new ProdutoDAO();
 			produtos = produtoDAO.listar("descricao");
 
-			itensVenda = new ArrayList<>();
+			itensVenda = new ArrayList<>();	
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Ocorreu um erro ao tentar carregar a tela de vendas");
 			erro.printStackTrace();
@@ -74,6 +112,8 @@ public class VendaBean implements Serializable {
 			itemVenda.setQuantidade(new Short(itemVenda.getQuantidade() + 1 + ""));
 			itemVenda.setPrecoParcial(produto.getPreco().multiply(new BigDecimal(itemVenda.getQuantidade())));
 		}
+		
+		calcular();
 	}
 
 	public void remover(ActionEvent evento) {
@@ -88,6 +128,30 @@ public class VendaBean implements Serializable {
 		
 		if(achou > -1){
 			itensVenda.remove(achou);
+		}
+		
+		calcular();
+	}
+	
+	public void calcular(){
+		venda.setPrecoTotal(new BigDecimal("0.00"));
+		
+		for(int posicao = 0; posicao < itensVenda.size(); posicao++){
+			ItemVenda itemVenda = itensVenda.get(posicao);
+			venda.setPrecoTotal(venda.getPrecoTotal().add(itemVenda.getPrecoParcial()));
+		}
+	}
+	
+	public void finalizar(){
+		try{
+			FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
+			funcionarios = funcionarioDAO.listarOrdenado();
+			
+			ClienteDAO clienteDAO = new ClienteDAO();
+			clientes = clienteDAO.listarOrdenado();
+		}catch(RuntimeException erro){
+			Messages.addGlobalError("Ocorreu um erro ao tentar finalizar a venda");
+			erro.printStackTrace();
 		}
 	}
 }
